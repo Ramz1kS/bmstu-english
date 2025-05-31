bdb3864432f439a10f32d500b37eacf6045061d6
Ramz1kS <skib1dimin1on2006@gmail.com>
2025-05-31 19:31:19 +0300

    work began

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="mainContainer">
+      <h1>ЧЁ, ОТВЕТОВ ЗАХОТЕЛ?</h1>
+      <p>подожди...</p>
+    </div>
+  )
+}
+
+export default App
