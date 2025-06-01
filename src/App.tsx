4c56ce96e8e0b3ed14c130d4efdb938714e49163
Ramz1kS <skib1dimin1on2006@gmail.com>
2025-06-01 12:51:29 +0300

    basic interface done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { CustomTextArea } from './components/CustomTextArea/CustomTextArea'
+import { Footer } from './components/Footer/Footer'
+import { TextAreaList } from './components/TextAreaList/TextAreaList'
 
 function App() {
   return (
     <div className="mainContainer">
-      <h1>ЧЁ, ОТВЕТОВ ЗАХОТЕЛ?</h1>
-      <p>подожди...</p>
+      <div className="greeting">
+        <h1>ЧЁ, ОТВЕТОВ ЗАХОТЕЛ?</h1>
+        <p>да пожалуйста! тебе нужно только...</p>
+      </div>
+      <TextAreaList></TextAreaList>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <div className={classes.footer}>
+      <div>
+        <a href="https://github.com/Ramz1kS" target="_blank">Автор форка</a>
+      </div>
+      <div>
+        <a href="https://github.com/siberianbearofficial" target="_blank">Автор парсера</a>
+      </div>
+      <div>
+        <a href="https://t.me/addstickers/iu7_secondB" target="_blank">От ИУ7 с любовью</a>
+      </div>
+    </div>
+  )
+}
