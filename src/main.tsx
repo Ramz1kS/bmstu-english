16e52b1de19690b73691704e7a637dc9d1ecd627
Ramz1kS <skib1dimin1on2006@gmail.com>
2025-06-02 13:14:40 +0300

    im desperate

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/bmstu-english/' : '/'}>
+  <BrowserRouter basename='/bmstu-english/'>
     <App />
   </BrowserRouter>
 )
